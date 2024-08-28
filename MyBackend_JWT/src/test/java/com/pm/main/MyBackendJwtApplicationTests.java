d4ae938d4f6743f24330b296db400007bf4c17db
prabhakar mandloi <prabhakarmandloi@gmail.com>
2024-08-28 22:14:02 +0530

    Initial Commit

---
+++ MyBackend_JWT/src/test/java/com/pm/main/MyBackendJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBackendJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
